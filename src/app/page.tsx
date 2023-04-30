b694fa901013972c8336be333b8199b9f32c1962
Ayush Seth <ayush-seth@outlook.com>
2023-05-01 02:43:54 +0530

    added scoreboard in header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col mx-auto mt-10 uppercase text-white leading-8 text-4xl rounded-xl border-2 w-[300px] p-6 border-slate-500">
-        <span>Rock</span>
-        <span>Paper</span>
-        <span>Scissors</span>
+      <div className="mx-auto flex justify-between mt-10 rounded-xl border-[3px] max-w-[650px] p-6 border-slate-500">
+        <div className="flex flex-col uppercase text-white leading-8 text-4xl ">
+          <span>Rock</span>
+          <span>Paper</span>
+          <span>Scissors</span>
+        </div>
+        <div className="bg-white rounded-lg flex flex-col items-center px-10 py-2">
+          <span className="uppercase tracking-widest text-[hsl(229,64%,46%)]">
+            Score
+          </span>
+          <span className="text-[hsl(229,25%,31%)] text-6xl font-bold">12</span>
+        </div>
       </div>
     </div>
   );
